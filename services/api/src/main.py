c911fe1742f5a4a3a667c90e5300dfabddae6111
Rodrigo Roldan Carlevarino <rrollpk@gmail.com>
2026-03-20 17:17:15 +0100

    fix: set Railway URL and fix CORS config

--- services/api/src/main.py
+++ services/api/src/main.py
@@ -13,7 +13,6 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
